b66027e4fddfa6a8de434a945124d02a2c4e4f76
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-05 14:29:47 +0100

    Create SensitiveDatasOnMessagesOvercomeSignalsByHighScaleKillSignals.h

---
+++ SensitiveDatasOnMessagesOvercomeSignalsByHighScaleKillSignals.h
@@ -0,0 +1,12 @@
+//if processes between two middleware are killed, we can froze datas that are executed by scripting if the messages that themselves are generated in nanoseconds
+//could stop running themselves. Then, there is a hole in ciphers that permit high scale digits datas. And then, it is not possible to stop
+//running threads by only one os system if this system is rejected physically by eletronic chips. The best thing here to do with our asserts would be to communicate with
+//all chips before programs contained in GDDR ram. Gddrram are then clamped by datas length. This nonsense here can be overcome if we send dummy messages before executing
+//devkits programs, between chips. So, we need several shared pointers in a chaotic system.
+
+
+//a dummy class is enough to track half sized datas thhat permit processes between two middlewares to run physically.
+class ContainerHalfEdgeGDDRRAMtocontainSharedPointers
+{
+
+}
