6a42bc3e951f0cbcb5b05d01819909b0e65bd3e9
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-05 06:10:35 +0100

    Create Limitsnumberdensitivedatasonaliasesovercomepointersaliasesassensituvetoo.h

---
+++ Limitsnumberdensitivedatasonaliasesovercomepointersaliasesassensituvetoo.h
@@ -0,0 +1,4 @@
+//two scale sensitive dztas may occur if the
+//pipeline is too mugh divided by extra shared pointers that is the cost
+//of a single pointer running in circle for too much processes that themsleves
+//cannot run as some frozen scalar states.
